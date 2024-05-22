e1400882e87fd76e20ae9138c9ebaaa63f141622
SAARANG <saarang3839@gmail.com>
2024-05-22 10:32:34 +0500

    new class

---
+++ index.js
@@ -0,0 +1,255 @@
+// alert("welcome to my laptop");
+
+// let firstName = "saarang";
+
+// lastname = "mursleen......";
+// alert(firstname + " " + lastname);
+
+// // variable rules
+
+// // 1 legal
+
+// var firstname = "nazeer";
+// var first_name1 = "imran";
+// var first_name2 = "mursleen";
+// var first_name3 = "saarang";
+// var first_name4 = "mubeen";
+
+// console.log(firstname + " " + first_name1 + " " + first_name2 + " " + first_name3 + " " + first_name4);
+
+// var num = "3839";
+//  console.log(num);
+// console.log(num)
+
+// Number
+// String
+// Boolean
+
+// let num = 123;
+// console.log(typeof(num))
+// num = 1234;
+// console.log(num)
+// num = 567
+// console.log(num)
+
+// const num = 66;
+
+// let num = "44"
+// let Num1 = "66";
+// alert(num + Num1)
+// console.log(num + Num1)
+
+// let firstname = "saarang";
+// let lastname = "jiskani";
+// console.log( firstname + " "  + lastname);
+
+// let email  = "abcd@gmail.com";
+// letName = "saarang";
+// let account = 12356346;
+// let etc
+// console.table([ email, name, account, etc]);
+
+// // Math operators = + - * / divided % modules
+
+// let num = 66;
+// let num4 = 2;
+// console.log(num + num4);
+
+// let num = 66;
+// let num5 = 100;
+// console.log(num - num5);
+
+// let Data = 66;
+// let Data2 = 2;
+// console.log(Data * Data2);
+
+// let DATA = 66;
+// let data1 = 2;
+// console.log(DATA / data1);   
+
+// let DATA2 = 100;
+// let data3 = 12;
+// let ADD = data2 % data3;
+// console.log(ADD);
+
+// let data = 11;
+// let data2 = 2;
+// let add = data % data2;
+// console.log(add);
+
+// letname = + prompt("what is your name......")
+// console.log(+ name + 5)
+// document.write(name)
+// let name = prompt("what is your number......")
+// console.log(name)
+
+// let randomNumber = Math.random();
+// console.log(randomNumber)
+
+// let rendomNumber = Math.random();
+// rendomNumber = rendomNumber * 10;
+// let rounded = Math.round(rendomNumber);
+// console.log(rounded);
+
+// // BODMAS
+// let num = [(5 + 5) * (5 * 17) - 10];
+// console.log(num)
+
+// letx=33;
+// console.log(x)
+// console.log(typeof String(x))
+
+// let Name = null;
+// console.log(name);
+
+// Boolean
+
+// let x = 33;
+// let y = false;
+// console.log(typeof y)
+
+// console.log(x + y)
+
+// let X = 33;
+// let Y = true;
+// console.log(x + 12 + y)
+
+// number + number  
+// string + number 
+// number + string 
+// string + string
+
+// NEW Worker
+
+// COMPERISON OPERATORS
+
+// ==  
+// value ko check karna
+
+// ===
+// VALUE KI TYPE CHECK KARNA
+
+// let num6 = 3; //data type num
+// let num7 = '3'; //string
+// console.log(num6 == num7)
+
+// let num8 = 3; //data type num
+// let num9 = 3; //string
+// console.log(num8 === num9)
+
+// // !=  , ==!
+
+// let num10 = 3; //data type num
+// let num2 = 3; //string
+
+// console.log('1' !== 2)
+
+// < greater than
+// > less than
+
+// console.log(3 < 6)
+// console.log(4 > 6)
+
+// let num = 76;
+// let num2 = 66;
+// console.log(num > num2)
+
+// let num1 = 56;
+// let num3 = 66;
+// console.log(num1 < num3)
+
+// <=  <==
+
+// let num1 = 155;
+// let num3 = 154;
+// console.log(num1 <= num3)
+// condition
+
+// let age = +prompt("inter your age");
+
+// if(age >= 18){
+//         console.log("you can apply cnic");}else{
+// console.log("you are not eligible");
+// }
+
+// let userNum = +prompt("inter your number");
+// let computernumber = Math.round(Math.random() * 5)
+
+// if(userNum === computernumber){
+//     console.log("you win");
+// }else{
+//     console.log("you loss !" + computernumber );
+
+// console.log(5 != 7);
+
+//  let Num = 55;
+// let num = 77;
+//     console.log(Num + num);
+ 
+ 
+
+// let ice = +prompt("inter your number");
+// if(ice === 100){
+//     console.log("ice-cream is corenetto");
+// }else if(ice === 500){
+//     console.log("ice-cream is corenetto family pack 1.5 LTR");
+// }else if(ice === 1000){
+//     console.log("ice-cream is corenetto family pack 2.5 LTR");
+// }else{
+//     console.log("your ice-cream is random");
+// }
+
+
+// switch (ice) {
+//     case 100:
+//         console.log("ice-cream is corenetto");
+//         break;
+//     case 500:
+//         console.log("ice-cream is corenetto family pack 1.5 LTR");
+//         break;
+//     case 1000:
+//         console.log("ice-cream is corenetto family pack 2.5 LTR");
+//         break;
+
+
+//     default:
+//         console.log("your ice-cream is random");
+// break;
+// }
+
+// let usernum = prompt("even number");
+// let computernumber = Math.round(Math.random() * 10);
+// if(computernumber % 2 === 0 && usernum === "even" ){
+//     console.log("you win !" + computernumber);
+// }
+// else if(computernumber % 2 !== 0 && usernum == "odd"){
+//     console.log("you win !");
+// }else{
+// console.log("you loss !" + computernumber);
+// }
+
+//inilization //let i = 1'
+//condition // i <= 100;
+//increment // i==;
+// for(let i = 1; i <= 100; i++){
+//     console.log("Hello World" + i);
+// }
+// for(let i = 1; i <= 30; i++){
+//     console.log("2 X " + i + " = " + 2 *i);
+// }
+
+//  let table = 6;
+//  for(let i = 1; i <= 10; i++){
+//          console.log(table + " X " + i + " = " + table *i);
+//      console.log(Math.round(Math.random() *10))
+//      console.log("hello world");
+//  }
+
+
+// let num = null
+// // let num1 = 77;
+// console.log(num);
+
+// let userNum = prompt("inter your number");
+// let computernumber = Math.round(Math.random() * 5);
+// if()
